2ec75bf5927706c0a8b67e6004ce2267415af891
abdul.wajid <abdul.wajid@sts.in>
2021-03-06 20:57:55 +0530

    order start msg added

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -9,6 +9,8 @@ import { PaymentCreatedListener } from "./events/listeners/payment-created-liste
 
 const start = async () => {
 
+    console.log('starting...');
+
     // For k8s env variables
     // if (!process.env.JWT_KEY) {
     //     throw new Error('JWT_KEY must be defined');
